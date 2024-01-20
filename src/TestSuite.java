799400175d2e41df96cb31b02525c7777aa016a4
Goh Koon Loong <92432620+GohKoonLoong@users.noreply.github.com>
2024-01-20 13:06:37 +0800

    new

---
+++ src/TestSuite.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ClassATest.class,
+        ClassBTest.class,
+        ClassCTest.class,
+        ClassETest.class,
+})
+public class TestSuite {
+
+}
